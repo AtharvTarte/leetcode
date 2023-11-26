d911793898007cb2b7b05c6b73f0267a8cf7da91
Atharv <athrvtrte@gmail.com>
2023-11-26 13:49:09 +0530

    q283

---
+++ 283.move-zeroes.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=283 lang=python3
+#
+# [283] Move Zeroes
+#
+
+# @lc code=start
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+        
+# @lc code=end
+
