4251d211c6b7097a0f24f0799b901c3b9a16e0f1
Atharv <athrvtrte@gmail.com>
2023-11-29 09:37:16 +0530

    array-easy

---
+++ 509.fibonacci-number.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=509 lang=python3
+#
+# [509] Fibonacci Number
+#
+
+# @lc code=start
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        return self.fib(n-1) + self.fib(n-2)
+        
+# @lc code=end
+
